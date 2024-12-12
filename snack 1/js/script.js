210cea9ed9cafdc361e175382f751c041a386a19
Umberto Amenta <umberto.amenta@hotmail.it>
2024-12-12 17:38:01 +0100

    feat: snack1

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,3 +1,39 @@
 /* Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore. */
+
+
+
+const bikes = [
+    {
+        brand : "Decathlon",
+        name : "Weg",
+        weight_kg : 15,
+    },
+    {
+        brand : "Btwin",
+        name : "Fold",
+        weight_kg : 13.5,
+    },
+    {
+        brand : "Riverside",
+        name : "R.120",
+        weight_kg : 14.5,
+    }
+]
+
+
+
+const allWeight = []
+for (let i = 0; i < bikes.length; i++) {
+    allWeight.push(bikes[i].weight_kg)
+}
+console.log(allWeight)
+
+let lowerWeight = allWeight[0]
+for (let i = 1; i < bikes.length; i++) {
+    if (allWeight[i] < lowerWeight) {
+        lowerWeight = allWeight[i]
+    }
+}
+console.log(lowerWeight)
